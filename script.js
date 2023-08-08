f42149099c52ece180c73aa2fbc3b1d063cb1712
Crod03 <crodriguez052103@gmail.com>
2023-08-07 23:40:59 -0400

    documents added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener ('DOMContentLoaded'), () => {
+    const gameContainer = documennt.querySelector('.game-container');
+    const playerPaddle = document.querySelector('.player');
+    const opposition = document.querySelector('opposition');
+    const soccer = document.querySelector('.soccer');
 
+    let soccerSpeedX = 5;
+    let soccerSpeedY = 5;
+    let playerScore = 0;
+    let oppositionScore = 0;
+
+}
