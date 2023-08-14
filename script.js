7462d26cbb55f41e2928d418018318acf474ee6b
Crod03 <Crodriguez052103@gmail.com git config --global user.name Crod03 git config --global user.password Cod.lif3git config --global user.email Crodriguez052103@gmail.com>
2023-08-14 17:32:30 -0400

    Score place holder

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameContainer = document.querySelector('.game-container');
     const playerPaddle = document.querySelector('.player');
-    const oppositionPaddle = document.querySelector('.opposition'); // Corrected variable name
+    const oppositionPaddle = document.querySelector('.opposition');
     const soccer = document.querySelector('.soccer');
 
     let soccerSpeedX = 5;
@@ -10,27 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
     let oppositionScore = 0;
 
     function update() {
-        // Move the ball
         const soccerRect = soccer.getBoundingClientRect();
-        const soccerX = soccerRect.left + soccerSpeedX;
-        const soccerY = soccerRect.top + soccerSpeedY;
+        let soccerX = soccerRect.left + soccerSpeedX;
+        let soccerY = soccerRect.top + soccerSpeedY;
 
-        // Check if it hits the walls
+        console.log(`soccerX: ${soccerX}, soccerY: ${soccerY}`); // Debug line
+
+        // Ball hits top or bottom walls
         if (soccerY < 0 || soccerY > gameContainer.clientHeight - soccerRect.height) {
             soccerSpeedY = -soccerSpeedY;
         }
 
-        // Check if it hits paddles
         const playerRect = playerPaddle.getBoundingClientRect();
-        const oppositionRect = oppositionPaddle.getBoundingClientRect(); // Corrected variable name
+        const oppositionRect = oppositionPaddle.getBoundingClientRect();
 
+        // Ball hits paddles
         if (soccerX < playerRect.right && soccerY + soccerRect.height > playerRect.top && soccerY < playerRect.bottom) {
             soccerSpeedX = -soccerSpeedX;
         } else if (soccerX + soccerRect.width > oppositionRect.left && soccerY + soccerRect.height > oppositionRect.top && soccerY < oppositionRect.bottom) {
             soccerSpeedX = -soccerSpeedX;
         }
 
-        // Check scores
+        // Ball crosses left or right boundaries
         if (soccerX < 0) {
             oppositionScore++;
             resetSoccer();
@@ -39,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
             resetSoccer();
         }
 
-        // Move paddles
+        // Player's paddle movement
         document.addEventListener('keydown', (event) => {
             if (event.key === 'w' && playerPaddle.offsetTop > 0) {
                 playerPaddle.style.top = `${playerPaddle.offsetTop - 10}px`;
@@ -48,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
 
-        // AI 
+        // AI opponent's movement
         const oppositionSpeed = 5;
         if (soccerSpeedX > 0 && soccerX > gameContainer.clientWidth / 2) {
             if (soccerY + soccerRect.height / 2 > oppositionRect.top + oppositionRect.height / 2) {
@@ -62,9 +63,11 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.player-score').textContent = playerScore;
         document.querySelector('.opposition-score').textContent = oppositionScore;
 
-        // Move ball
+        // Move the ball
         soccer.style.left = `${soccerX}px`;
         soccer.style.top = `${soccerY}px`;
+
+        console.log(`Ball position: left=${soccer.style.left}, top=${soccer.style.top}`); // Debug line
 
         // Continue the game loop
         requestAnimationFrame(update);
@@ -76,8 +79,6 @@ document.addEventListener('DOMContentLoaded', () => {
         soccer.style.top = `${gameContainer.clientHeight / 2 - soccer.clientHeight / 2}px`;
     }
 
-    // game loop
+    // Start the game loop
     update();
 });
-
-
